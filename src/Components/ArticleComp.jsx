e6d3f91f72e866a66a4d8ccf19c2fbade142ef2a
Riley Horita <rileyhorita@berkeley.edu>
2024-01-18 20:16:44 -0800

    ok

--- src/Components/ArticleComp.jsx
+++ src/Components/ArticleComp.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './ArticleComp.css';
     function createArticle({heading, description, href , artistImage}){
         return (
